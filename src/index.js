c0da78cea956f03cc08b9e6d65859fed766341c7
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-08-15 15:47:35 +0100

    Todo List structured

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import './style.css'
+import './styles/style.css'
+import {treeDots, refresh} from './modules/svg';
+/* import refresh  from './assets/refresh.svg' */
+
+const tasks = [
+    { description: 'Buy groceries', completed: false, index: 1 },
+    { description: 'Read a book', completed: true, index: 2 },
+    { description: 'Go for a walk', completed: false, index: 3 },
+];
+document.getElementById('refreshSvg').innerHTML = refresh;
+const taskList = document.getElementById('task-list');
+
+const renderTasks = () => {
+    taskList.innerHTML = tasks
+        .map(task => `
+            <li class="task ${task.completed ? 'completed' : ''}">
+                <input type="checkbox" class="completed-checkbox" ${task.completed ? 'checked' : ''}>
+                <span class="task-description">${task.description}</span>
+                <button class="treeDots">${treeDots}</button>
+            </li>
+        `)
+        .join('');
+}
+
+renderTasks();
--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,29 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: {
-        bund: path.resolve(__dirname, 'src/index.js'),
+        bundle: path.resolve(__dirname, 'src/index.js'),
     },
     output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     plugins: [
         new HtmlwebpackPlugin({
-            title: 'Output Management',
+            title: 'To-Do List',
             template: 'src/index.html',
         }),
     ],
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
+      devtool: 'source-map',
+      devServer: {
+          static: {
+              directory: path.resolve(__dirname, 'dist')
+          },
+          port: 3000,
+          open: true,
+          hot: true,
+          compress: true,
+          historyApiFallback: true,
       },
     module: {
         rules: [
@@ -27,6 +34,20 @@ module.exports = {
             test: /\.css$/i,
             use: ['style-loader', 'css-loader'],
           },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env'],  
+                }
+            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
         ],
       },
 }
