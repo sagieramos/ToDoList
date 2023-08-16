7b9898cb37e2bd60c705a20e1002bc8d52002953
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-08-16 17:53:21 +0100

    created class structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
-import './styles/style.css';
-import init from './modules/renderPage.js';
+import TaskManager from "./modules/renderPage";
 
-document.addEventListener('DOMContentLoaded', init);
+const taskManager = new TaskManager();
+
+const todoForm = document.getElementById('todoForm');
+const clearCompletedButton = document.getElementById('clearCompleted');
+
+console.log('Todo form:', todoForm);
+console.log('Clear completed button:', clearCompletedButton);
+
+todoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = todoForm.querySelector('input[type="text"]');
+    const description = input.value.trim();
+    
+    if (description !== '') {
+        taskManager.addTask(description);
+        input.value = '';
+    }
+});
+
+
+window.addEventListener('click', (e) => {
+    const it = ([...document.querySelectorAll('.task-description')])
+    it.forEach((item) => {
+        console.log(item.innerHTML);
+    })
+    const { target } = e;
+    console.log(target);
+    if (target.matches('#clearCompleted')) {
+        taskManager.clearCompleted();
+ /*    console.log('Clear completed button clicked '+ target);
+    taskManager.tasks = taskManager.tasks.filter(task => !task.completed);
+    taskManager.updateTaskIndexes();
+    taskManager.renderTasks(); */
+    } else if (target.matches('input[type="checkbox"]')) {
+        taskManager.checkbox(target);
+    }
+});
--- src/modules/renderPage.js
+++ src/modules/renderPage.js
@@ -1,27 +1,88 @@
 import { treeDots, refresh } from './svg.js';
 
-const tasks = [
-  { description: 'Come to Nigeria', completed: false, index: 1 },
-  { description: 'Read shell and Linux', completed: true, index: 2 },
-  { description: 'Go, buy a drink', completed: false, index: 3 },
-];
-const taskList = document.getElementById('task-list');
+import '../styles/style.css';
 
-const renderTasks = () => {
-  taskList.innerHTML = tasks
-    .map((task) => `
-            <li class="task ${task.completed ? 'completed' : ''}">
-                <input type="checkbox" class="completed-checkbox" ${task.completed ? 'checked' : ''}>
-                <span class="task-description">${task.description}</span>
-                <button class="treeDots">${treeDots}</button>
-            </li>
-        `)
-    .join('');
+const createTaskElement = (task) => {
+  const taskElement = document.createElement('li');
+  taskElement.className = `task ${task.completed ? 'completed' : ''}`;
+  taskElement.innerHTML = `
+      <input type="checkbox" class="completed-checkbox" ${task.completed ? 'checked' : ''}>
+      <span class="index">${task.index}.</span>
+      <span class="task-description">${task.description}</span>
+      <button class="treeDots">${treeDots}</button>
+  `;
+  
+  return taskElement;
 };
+class TaskManager {
+    constructor() {
+        this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        this.taskList = document.getElementById('task-list');
+        
+        this.renderTasks();
+    }
+    
+    saveTasksToLocalStorage() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
 
-const int = () => {
-  document.getElementById('refreshSvg').innerHTML = refresh;
-  renderTasks();
-};
+    updateTaskIndexes() {
+      this.tasks.forEach((task, index) => {
+          task.index = index + 1;
+      });
+  }
+    renderTasks() {
+      this.updateTaskIndexes();
+        this.taskList.innerHTML = '';
+        
+        this.tasks.forEach((task, index) => {
+            const taskElement = createTaskElement(task, index);
+            this.taskList.appendChild(taskElement);
+        });
+        
+        this.saveTasksToLocalStorage();
+    }
+    
 
-export default int;
+
+    checkbox (target) {
+      if (target.checked) {
+        target.parentNode.classList.add('completed');
+      } else {
+        target.parentNode.classList.remove('completed');
+      }
+    }
+    
+    clearCompleted() {
+      const completedCheckboxes = [...document.querySelectorAll('.completed-checkbox')];
+      const newTasks = this.tasks.filter((task, index) => {
+          return !completedCheckboxes[index].checked;
+      });
+      this.tasks = newTasks;
+      this.renderTasks();
+    }
+
+    addTask(description) {
+        const newTask = {
+            description: description,
+            completed: false,
+            index: this.tasks.length + 1,
+        };
+        this.tasks.push(newTask);
+        this.renderTasks();
+    }
+    
+    deleteTask(index) {
+        this.tasks.splice(index, 1);
+        this.updateTaskIndexes();
+        this.renderTasks();
+    }
+    
+    updateTaskCompletion(index, completed) {
+        this.tasks[index].completed = completed;
+        this.saveTasksToLocalStorage();
+        this.renderTasks();
+    }
+}
+
+export default TaskManager;
