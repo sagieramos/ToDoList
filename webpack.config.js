3ab6d03fef56444ce828caadc905873e18747a36
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-08-18 08:57:25 +0100

    deploy using gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     filename: '[name][contenthash].js',
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   plugins: [
