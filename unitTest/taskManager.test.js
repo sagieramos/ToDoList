52cae8c346aed6dfbb282c70f29464c1a7ca2c73
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-08-24 14:21:55 +0100

    Unit test part 2

--- unitTest/taskManager.test.js
+++ unitTest/taskManager.test.js
@@ -2,8 +2,10 @@ import { JSDOM } from 'jsdom';
 import { TaskManager } from '../src/modules/util.js';
 
 const mockedTasksArray = [
-  { description: 'Task 1', completed: false, index: 1 },
-  { description: 'Task 2', completed: true, index: 2 },
+  { description: 'Task 1', completed: true, index: 1 },
+  { description: 'Task 2', completed: false, index: 2 },
+  { description: 'Task 3', completed: false, index: 3 },
+  { description: 'Task 4', completed: false, index: 4 },
 ];
 
 describe('TaskManager', () => {
@@ -61,13 +63,15 @@ describe('TaskManager', () => {
         taskManager.addTask(task);
       });
 
-      expect(taskManager.tasks[0].index).toBe(1);
+      expect(taskManager.tasks[0].description.index).toBe(1);
       expect(taskManager.tasks[0].description.completed).toBe(false);
       expect(taskManager.tasks[0].description.description).toBe('Task 1');
-      expect(taskManager.tasks[1].index).toBe(2);
+
+      expect(taskManager.tasks[1].description.index).toBe(2);
       expect(taskManager.tasks[1].description.description).toBe('Task 2');
       expect(taskManager.tasks[1].description.completed).toBe(true);
-      expect(taskManager.tasks[2].index).toBe(3);
+
+      expect(taskManager.tasks[2].description.index).toBe(3);
       expect(taskManager.tasks[2].description.description).toBe('Task 3');
       expect(taskManager.tasks[2].description.completed).toBe(false);
     });
@@ -81,7 +85,7 @@ describe('TaskManager', () => {
 
       taskManager.renderTasks();
 
-      expect(document.querySelectorAll('.task')).toHaveLength(5);
+      expect(document.querySelectorAll('.task')).toHaveLength(7);
     });
   });
 
@@ -94,7 +98,23 @@ describe('TaskManager', () => {
       taskManager.getIndex(1);
       taskManager.deleteTask();
 
-      expect(document.querySelectorAll('.task')).toHaveLength(4);
+      expect(document.querySelectorAll('.task')).toHaveLength(6);
     });
+  });
+
+  describe('Test part 2', () => {
+
+    test('Status in the task-list is true and false respectively', () => {
+      expect(taskManager.tasks[0].completed).toBe(true);
+      expect(taskManager.tasks[1].completed).toBe(false);
+    });
+
+    test('Contents in task-list is "Task 1" and "Task 2 respectively"', () => {
+      expect(taskManager.tasks[0].description).toBe('Task 1');
+      expect(taskManager.tasks[1].description).toBe('Task 2');
+    });
+
+    //
+
   });
 });
